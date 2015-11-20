391ff9dfcb8182bb88272aa66f3a4682506be531
mazakk94 <mazakk94@gmail.com>
2015-11-20 13:31:08 +0100

    second commit

---
+++ Samples/CameraControlSample/FormCameraControlSample.cs
@@ -0,0 +1,221 @@
+﻿#region License of Sample
+
+/*
+CameraControlSample - is a simple sample of CameraControl usage
+Copyright (C) 2013
+https://github.com/free5lot/Camera_Net
+
+While the Camera_Net library is covered by LGPL, 
+this sample is released as PUBLIC DOMAIN.
+So, you can use code from this sample in your 
+free or proprietary project without any limitations.
+
+It is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+#endregion
+
+namespace CameraControlSample
+{
+    #region Using directives
+
+    using System;
+    using System.Windows.Forms;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.Drawing.Drawing2D;
+    using System.Drawing.Text;
+    using System.Runtime.InteropServices.ComTypes;
+
+    using Camera_NET;
+    using DirectShowLib;
+
+    #endregion
+
+    // FormCameraControlSample
+    public partial class FormCameraControlSample : Form
+    {
+
+
+        public static Bitmap MakeGrayscale3(Bitmap original)
+        {
+            //create a blank bitmap the same size as original
+            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+
+            //get a graphics object from the new image
+            Graphics g = Graphics.FromImage(newBitmap);
+
+            //create the grayscale ColorMatrix
+            ColorMatrix colorMatrix = new ColorMatrix(
+               new float[][] 
+      {
+         new float[] {.3f, .3f, .3f, 0, 0},
+         new float[] {.59f, .59f, .59f, 0, 0},
+         new float[] {.11f, .11f, .11f, 0, 0},
+         new float[] {0, 0, 0, 1, 0},
+         new float[] {0, 0, 0, 0, 1}
+      });
+
+            //create some image attributes
+            ImageAttributes attributes = new ImageAttributes();
+
+            //set the color matrix attribute
+            attributes.SetColorMatrix(colorMatrix);
+
+            //draw the original image on the new image
+            //using the grayscale color matrix
+            g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height),
+               0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+
+            //dispose the Graphics object
+            g.Dispose();
+            return newBitmap;
+        }
+
+        #region Variables
+
+        // Camera object
+        //private Camera _Camera;
+
+        // Camera choice
+        private CameraChoice _CameraChoice = new CameraChoice();
+
+        #endregion
+
+        #region Winforms stuff
+
+        // Constructor
+        public FormCameraControlSample()
+        {
+            InitializeComponent();
+        }
+
+        // On load of Form
+        private void FormCameraControlSample_Load(object sender, EventArgs e)
+        {
+            // Fill camera list combobox with available cameras
+            FillCameraList();
+
+            // Select the first one
+            if (comboBoxCameraList.Items.Count > 0)
+            {
+                comboBoxCameraList.SelectedIndex = 0;
+            }
+
+            // Fill camera list combobox with available resolutions
+            FillResolutionList();
+        }
+
+        // On close of Form
+        private void FormMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // Close camera
+            cameraControl.CloseCamera();
+        }
+
+        #endregion
+
+        #region Camera and resolution selection
+
+        private void FillCameraList()
+        {
+            comboBoxCameraList.Items.Clear();
+
+            _CameraChoice.UpdateDeviceList();
+
+            foreach (var camera_device in _CameraChoice.Devices)
+            {
+                comboBoxCameraList.Items.Add(camera_device.Name);
+            }
+        }
+
+        private void FillResolutionList()
+        {
+            comboBoxResolutionList.Items.Clear();
+
+            if (!cameraControl.CameraCreated)
+                return;
+
+            ResolutionList resolutions = Camera.GetResolutionList(cameraControl.Moniker);
+
+            if (resolutions == null)
+                return;
+
+            int index_to_select = -1;
+
+            for (int index = 0; index < resolutions.Count; index++)
+            {
+                comboBoxResolutionList.Items.Add(resolutions[index].ToString());
+
+                if (resolutions[index].CompareTo(cameraControl.Resolution) == 0)
+                {
+                    index_to_select = index;
+                }
+            }
+
+            // select current resolution
+            if (index_to_select >= 0)
+            {
+                comboBoxResolutionList.SelectedIndex = index_to_select;
+            }
+        }
+
+        private void comboBoxCameraList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxCameraList.SelectedIndex < 0)
+            {
+                cameraControl.CloseCamera();
+            }
+            else
+            {
+                // Set camera
+                cameraControl.SetCamera(_CameraChoice.Devices[comboBoxCameraList.SelectedIndex].Mon, null);
+                //SetCamera(_CameraChoice.Devices[ comboBoxCameraList.SelectedIndex ].Mon, null);
+            }
+
+            FillResolutionList();
+        }
+
+        private void comboBoxResolutionList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!cameraControl.CameraCreated)
+                return;
+
+            int comboBoxResolutionIndex = comboBoxResolutionList.SelectedIndex;
+            if (comboBoxResolutionIndex < 0)
+            {
+                return;
+            }
+            ResolutionList resolutions = Camera.GetResolutionList(cameraControl.Moniker);
+
+            if ( resolutions == null )
+                return; 
+
+            if ( comboBoxResolutionIndex >= resolutions.Count )
+                return; // throw
+
+            if (0 == resolutions[comboBoxResolutionIndex].CompareTo(cameraControl.Resolution))
+            {
+                // this resolution is already selected
+                return;
+            }
+
+            // Recreate camera
+            //SetCamera(_Camera.Moniker, resolutions[comboBoxResolutionIndex]);
+            cameraControl.SetCamera(cameraControl.Moniker, resolutions[comboBoxResolutionIndex]);
+
+        }
+
+        #endregion
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = MakeGrayscale3(cameraControl.Camera.SnapshotOutputImage());
+        }
+
+    }
+}
